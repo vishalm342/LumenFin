7fa33872f409d41ee786fca975fb3eddf0b297b5
vishalm342 <vishalm8656@gmail.com>
2026-01-14 23:44:35 +0530

    Remove .next build artifacts from git tracking

--- .next/dev/static/chunks/src_app_dashboard_documents_page_tsx_6b06d693._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_10481735._.js",
-  "static/chunks/src_components_ac4647be._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_dashboard_layout_tsx_a65b1859._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_d0670c7f._.js",
-  "static/chunks/src_982fd2db._.js"
-],
-    source: "dynamic"
-});
